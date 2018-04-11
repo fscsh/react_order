399a8ac49885c4bf359efb5c8e01f4ec4a85bdb8
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-04-11 00:58:11 -0700

    add some CSS

--- src/lib/products.mock.js
+++ src/lib/products.mock.js
@@ -2,97 +2,97 @@ export default [
   {
     "_id": "599598ac9040faa901d2bc76",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken1",
     "price": 33.07
   },
   {
     "_id": "599598acb5211750d129dd50",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken2",
     "price": 30.46
   },
   {
     "_id": "599598ac5c2e3bf27c004a79",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken3",
     "price": 73.18
   },
   {
     "_id": "599598acfe9c6acf7ed31d5f",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken4",
     "price": 43.3
   },
   {
     "_id": "599598ac632644a16e954eeb",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken5",
     "price": 93.61
   },
   {
     "_id": "599598ac2f70ac1ab85b301e",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken6",
     "price": 45.07
   },
   {
     "_id": "599598aced479eec77c4c7ee",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken7",
     "price": 37.3
   },
   {
     "_id": "599598acbc7cc424c4fae233",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken8",
     "price": 99.18
   },
   {
     "_id": "599598ac9843d314d397f03f",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken9",
     "price": 80.9
   },
   {
     "_id": "599598ace6f3cd8e79b50d79",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken10",
     "price": 49.89
   },
   {
     "_id": "599598ac28f12c52841110bf",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken11",
     "price": 74.91
   },
   {
     "_id": "599598ac961dff7c6a3605ea",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken12",
     "price": 37.24
   },
   {
     "_id": "599598acdb94e73f93dd539e",
     "picture":  "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken13",
     "price": 43.33
   },
   {
     "_id": "599598ac51bfae22431e3ebb",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken14",
     "price": 58.5
   },
   {
     "_id": "599598ac4eca01050bbee24d",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken15",
     "price": 58.77
   },
   {
     "_id": "599598ac7cbf78bdefaa64a9",
     "picture": "https://drop.ndtv.com/albums/COOKS/bbq-recipes/barbecuechicken_640x480.jpg",
-    "name": "chicken",
+    "name": "chicken16",
     "price": 58.13
   }
 ];
